be78d29e64a8f3771e1c0c8aad52a90a4d28567e
Christian <LordNature@users.noreply.github.com>
2017-03-09 04:29:56 -0600

    Update steam.js

--- steam.js
+++ steam.js
@@ -1,6 +1,4 @@
-//Steam Fetching Gmod Stuff
 function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode ) {
-	//document.getElementById( "servername" ).innerHTML = servername;
-	document.getElementById( "servermap" ).innerHTML = mapname;
-	document.getElementById( "servergm" ).innerHTML = gamemode;
+	document.getElementById("servermap").innerHTML = mapname;
+	document.getElementById("servergm").innerHTML = gamemode;
 }
